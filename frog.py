b0873f2e8f0112986aa2c9283eb255391051b9a9
HinaWatanabe <watanabehina.0806@gmail.com>
2025-07-05 19:55:05 +0900

    基本機能の実装

--- frog.py
+++ frog.py
@@ -1,30 +1,58 @@
 #ユーザーインタフェース
 
+from frog_model import Frog
+
 def main():
     print("🐸　カエルを観察するアプリ　🐸")
 
-    days = 1
+    putname = input("カエルの名前を決めてね🐸")
+    frog = Frog(name = putname)
+    
 
     while True:
-        print(f"\n{days}日目🐸")
+        print(f"\n{frog.days}日目🐸")
         print("\nコマンドを選択してください：")
         print("1. 観察記録を見る")
         print("2. 餌やり")
-        print("3. アプリ終了")
+        print("3. 仲良くする")
+        print("4. アプリ終了")
 
         choice = input("何をしますか？（番号を入力）：")
 
         if choice == "1":
-            print("[観察記録]まだこれから")
+            status = frog.get_status()
+            print(f"\n[{frog.name}のステータスです]\n")
+            for key, val in status.items():
+                print(f"{key}: {val}")
+
         elif choice == "2":
-            print("[餌やり]まだこれから")
+            frog.feed()
+            print("餌をあげました！\n")
+            print(f"{frog.name}は喜んでいます！")
+
         elif choice == "3":
+            print("仲良くしよう！コマンドを選択してください：")
+            print("1. 触れ合う")
+            print("2. 会話する")
+            fr_choice = input("何をしたいですか？（番号を入力）：")
+
+            if fr_choice == "1":
+                frog.touch()
+                print("触れ合いました！親密度UP")
+
+            elif fr_choice == "2":
+                frog.talk()
+
+            else:
+                print("[無効]1~2で選択してください")
+
+        elif choice == "4":
             save_data()
             print("これまでの行動を記録してアプリを終了します\n")
             print("またきてね🐸")
             break
         else:
-            print("[無効]1~3で選択してください")
+            print("[無効]1~4で選択してください")
 
         days += 1
 
---
+++ frog_model.py
@@ -0,0 +1,60 @@
+import random
+
+#カエルの状態
+class Frog:
+    def __init__(self, name):
+        self.name =name
+        self.days = 1
+        self.weight = 10.0
+        self.fullstom = 100
+        self.bodytemp = 20.0
+        self.friend = 0
+
+
+    def feed(self, food=10):
+            self.fullstom = min(100, self.fullstom + food)
+            self.weight += 1  # 餌10で1g増える
+    
+    def pass_day(self):
+        self.days += 1
+        self.fullstom = max(0, self.fullstom - 10)
+        if self.fullstom < 30:
+            self.weight -= 0.2  # 空腹だと痩せる
+            self.friend -= 
+            
+        elif self.fullstom > 60:
+            self.friend += 3
+
+        else:
+            pass
+    
+    def get_status(self):
+        return {
+            "日数": self.days,
+            "名前": self.name,
+            "親密度": f"{self.friend:.1f}/100",
+            "体重": f"{self.weight:.1f}g",
+            "満腹度": self.fullstom,
+            "体温": f"{self.bodytemp:.1f}℃"
+        }
+    
+    def touch(self):
+        self.friend += 5
+    
+    def talk(self):
+        word = input("話しかけてみよう！（話したいことを入力）：")
+
+        if "ケロ" in word or "ゲロ" in word or "ゲコ" in word:
+            responses = [
+                "ケロケロッ！",
+                "ケロケーロ、ケロ！",
+                "グワッグワッ",
+                "ゲコゲコゲコ！"
+            ]
+            print(random.choice(responses))
+            self.friend += 5
+        
+        else:
+            print(f"{self.name}は首をかしげている🐸？")
+    
+    
